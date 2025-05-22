dd73143e0feda24b0bdcc624450eacddf8ce4d14
111858-DelfinoDavid <111858@tecnicatura.frc.utn.edu.ar>
2025-05-22 20:35:49 -0300

    test realizados

--- src/test/java/org/example/parking/EstacionamientoTest.java
+++ src/test/java/org/example/parking/EstacionamientoTest.java
@@ -1,5 +1,6 @@
 package org.example.parking;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
@@ -7,14 +8,40 @@ import static junit.framework.Assert.assertTrue;
 
 public class EstacionamientoTest {
 
+    private Estacionamiento estacionamiento;
+    private Vehiculo vehiculo;
+    private Cliente cliente;
+
+
+    @Before
+    public void setup(){
+        estacionamiento=new Estacionamiento();
+        vehiculo=new Vehiculo("AAA","Chevrolet Meriva", Vehiculo.Tipo.AUTO);
+        cliente=new Cliente("111","David");
+        estacionamiento.ingresarVehiculo(cliente.getDni(),cliente.getNombre(),vehiculo);
+    }
+
     @Test
     public void testRetirarVehiculo() throws Exception {
-        //TODO test
+        //DONE test
+        setup();
+
+        Ticket ticket=estacionamiento.retirarVehiculo("AAA");
+
+        assertEquals(ticket.getVehiculo().getPatente(),vehiculo.getPatente());
     }
 
     @Test
     public void testCalcularPrecio() throws Exception {
-        // TODO test
+        // DONE test
+        Ticket ticket=estacionamiento.retirarVehiculo("AAA");
+        ticket.marcarSalida();
+
+        double precio=ticket.calcularPrecio();
+
+        assertEquals(300.0,precio,100.0);
+
+
     }
 
 }
